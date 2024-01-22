d83ee67d52f3dba97d09693d4165d2a3abbb258a
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-01-22 07:40:56 +0200

    Update GameJolt.hx

--- hxgamejolt/GameJolt.hx
+++ hxgamejolt/GameJolt.hx
@@ -600,4 +600,5 @@ class GameJolt
 	}
 }
 
-private abstract OneOfTwo<T1, T2>(Dynamic) from T1 from T2 to T1 to T2 {}
+@:noCompletion
+private class abstract OneOfTwo<T1, T2>(Dynamic) from T1 from T2 to T1 to T2 {}
