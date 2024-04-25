dd074b677bafc9eff42b41552022dad3d9cc04a9
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-04-25 15:00:00 +0300

    Create OneOfTwo.hx

---
+++ hxgamejolt/util/OneOfTwo.hx
@@ -0,0 +1,6 @@
+package hxgamejolt.util;
+
+/**
+ * @see https://github.com/HaxeFlixel/flixel/blob/master/flixel/util/typeLimit/OneOfTwo.hx
+ */
+abstract OneOfTwo<T1, T2>(Dynamic) from T1 from T2 to T1 to T2 {}
